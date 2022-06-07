8510f8d81dfb65361c1054c972e3b560d72e0f6d
uor16x <u0r.home@gmail.com>
2022-06-07 16:23:14 +0300

    ResultItem widget;

--- lib/keygen/gen_result.dart
+++ lib/keygen/gen_result.dart
@@ -1,5 +1,50 @@
 import 'package:flutter/material.dart';
 
+class _ResultItem extends StatelessWidget {
+  final String label;
+  final String text;
+  final String button;
+  final Function onPress;
+
+  const _ResultItem({
+    required this.label,
+    required this.text,
+    required this.button,
+    required this.onPress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(fontSize: 16),
+          textAlign: TextAlign.left,
+        ),
+        Text(
+          text,
+          style: const TextStyle(fontSize: 21),
+          textAlign: TextAlign.left,
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.white30,
+          ),
+          onPressed: () {
+            onPress();
+          },
+          child: Text(
+            button,
+            style: const TextStyle(fontSize: 24),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class GenResult extends StatelessWidget {
   final String resultText;
   final String resultKey;
@@ -32,13 +77,20 @@ class GenResult extends StatelessWidget {
             ),
             textAlign: TextAlign.left,
           ),
+          const Text(
+            'Encoded result',
+            style: TextStyle(
+              fontSize: 21,
+            ),
+            textAlign: TextAlign.left,
+          ),
           ElevatedButton(
-            // style: ElevatedButton.styleFrom(
-            //   primary: Colors.black,
-            // ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.white30,
+            ),
             onPressed: () {},
             child: const Text(
-              'Submit',
+              'Copy',
               style: TextStyle(fontSize: 24),
             ),
           ),
