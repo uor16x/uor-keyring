5bb71fe026c2fdb402ee0c9221066131ffc87c28
uor16x <u0r.home@gmail.com>
2022-06-16 23:55:58 +0300

    folder struct move;

--- lib/widgets/keygen/actions/concat.dart
+++ lib/widgets/keygen/actions/concat.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uor_keyring/service/transform/concat.dart';
+import 'package:uor_keyring/transform/concat.dart';
 import 'package:uor_keyring/shared/action_result.dart';
 import 'package:uor_keyring/widgets/keygen/actions/select_input.dart';
 import 'package:uor_keyring/widgets/keygen/actions/transform_action.dart';
--- lib/widgets/keygen/actions/substr.dart
+++ lib/widgets/keygen/actions/substr.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uor_keyring/service/transform/substr.dart';
+import 'package:uor_keyring/transform/substr.dart';
 import 'package:uor_keyring/shared/action_result.dart';
 import 'package:uor_keyring/shared/toast.dart';
 import 'package:uor_keyring/widgets/keygen/actions/select_input.dart';
