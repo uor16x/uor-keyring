03dc36b9a89d6ce8e1bacb1ca6c3264d445cfffc
uor16x <u0r.home@gmail.com>
2022-06-19 22:49:38 +0300

    Basic switch attach widget;

--- lib/widgets/keygen/actions/attach.dart
+++ lib/widgets/keygen/actions/attach.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:uor_keyring/extensions.dart';
 import 'package:uor_keyring/shared/action_result.dart';
+import 'package:uor_keyring/shared/ordered_string_item.dart';
+import 'package:uor_keyring/widgets/keygen/actions/select_input.dart';
+import 'package:uor_keyring/widgets/shared/styles.dart';
+
+enum AttachPosition { before, after }
+
+class _SwitchAttachPosition extends StatefulWidget {
+  @override
+  State<_SwitchAttachPosition> createState() => _SwitchAttachPositionState();
+}
+
+class _SwitchAttachPositionState extends State<_SwitchAttachPosition> {
+  AttachPosition value = AttachPosition.after;
+
+  void setValue(bool switchValue) {
+    setState(() {
+      value = switchValue ? AttachPosition.after : AttachPosition.before;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(value: value == AttachPosition.after, onChanged: setValue);
+  }
+}
 
 class Attach extends StatefulWidget {
   final List<ActionLogItem> inputs;
@@ -12,8 +37,27 @@ class Attach extends StatefulWidget {
 }
 
 class _AttachState extends State<Attach> {
+  OrderedStringItem? item;
+
+  void setItem(OrderedStringItem? value) {
+    setState(() {
+      item = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: Styles.boxDecoration,
+      padding: Styles.padding(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SelectInput(inputs: widget.inputs, onSelect: setItem),
+          Styles.emptySpace(),
+          SelectInput(inputs: widget.inputs, onSelect: setItem),
+        ],
+      ),
+    );
   }
 }
