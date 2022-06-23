eb04e34fdaa95eef940b4023f4ccff9ca4c14c97
uor16x <u0r.home@gmail.com>
2022-06-23 19:41:59 +0300

    key types;

--- lib/transform/transform.dart
+++ lib/transform/transform.dart
@@ -1,4 +1,8 @@
 import 'package:uor_keyring/shared/ordered_string_item.dart';
+import 'package:uor_keyring/transform/attach.dart';
+import 'package:uor_keyring/transform/concat.dart';
+import 'package:uor_keyring/transform/none.dart';
+import 'package:uor_keyring/transform/substr.dart';
 
 enum TransformActionType {
   none,
@@ -17,5 +21,11 @@ abstract class Transformable {
   final TransformActionType key = TransformActionType.none;
   final OrderedStringItem input = OrderedStringItem(0, '');
   final List args = [];
+  static Map<TransformActionType, Type> keyTypes = {
+    TransformActionType.none: NoneTransform,
+    TransformActionType.attach: AttachTransform,
+    TransformActionType.concat: ConcatTransform,
+    TransformActionType.substr: SubstrTransform,
+  };
   String transform();
 }
