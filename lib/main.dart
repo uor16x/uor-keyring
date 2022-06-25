00758849b8ecdebb122a7b93c70819e6b3297509
uor16x <u0r.home@gmail.com>
2022-06-26 01:10:06 +0300

    Styling moved;

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
+import 'package:uor_keyring/theme.dart';
 import 'widgets/home.dart';
 
 Future testWindowFunctions() async {
@@ -21,44 +22,12 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const Color mainColor = Color.fromARGB(128, 40, 47, 56);
-  static const Color mainOverlappingColor = Color.fromRGBO(30, 35, 42, 1);
-  static const Color secondaryColor = Colors.white;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Uor Keyring',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        cardColor: mainColor,
-        canvasColor: mainOverlappingColor,
-        bottomSheetTheme: const BottomSheetThemeData(
-          backgroundColor: mainOverlappingColor,
-        ),
-        indicatorColor: secondaryColor,
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: secondaryColor,
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            padding: MaterialStateProperty.all(
-              const EdgeInsets.only(top: 12, bottom: 12),
-            ),
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-                side: const BorderSide(color: secondaryColor, width: 1.0),
-              ),
-            ),
-            backgroundColor: MaterialStateProperty.all(mainColor),
-          ),
-        ),
-        appBarTheme: const AppBarTheme(
-          color: mainColor,
-        ),
-        scaffoldBackgroundColor: mainColor,
-      ),
+      theme: getTheme(),
       home: const Home(),
     );
   }
---
+++ lib/theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+const Color mainColor = Color.fromARGB(128, 40, 47, 56);
+const Color mainOverlappingColor = Color.fromRGBO(30, 35, 42, 1);
+const Color secondaryColor = Color.fromARGB(225, 255, 255, 255);
+
+ThemeData getTheme() {
+  return ThemeData.dark().copyWith(
+    cardColor: mainColor,
+    canvasColor: mainOverlappingColor,
+    bottomSheetTheme: const BottomSheetThemeData(
+      backgroundColor: mainOverlappingColor,
+    ),
+    indicatorColor: secondaryColor,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: secondaryColor,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        padding: MaterialStateProperty.all(
+          const EdgeInsets.only(top: 12, bottom: 12),
+        ),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+            side: const BorderSide(color: secondaryColor, width: 1.0),
+          ),
+        ),
+        backgroundColor: MaterialStateProperty.all(mainColor),
+      ),
+    ),
+    appBarTheme: const AppBarTheme(
+      color: mainColor,
+    ),
+    scaffoldBackgroundColor: mainColor,
+  );
+}
